dcbf61bfd55fa4e9e0c358081b35e97610a1a151
rtaylor456 <ruth.eloisa.456@gmail.com>
2024-04-23 19:46:51 -0600

    update files in package with documentation

--- R/analyze_nas.R
+++ R/analyze_nas.R
@@ -1,5 +1,22 @@
-
-
+#' Analyze and produce information on NAs in dataset.
+#'
+#' This function examines the inputted dataset for NA values and outputs a table
+#'     of counts and proportions of NAs for each appropriate variable in a data
+#'     frame within the R console and a generated csv file, written to the
+#'     current working directory.
+#'
+#' @param df The data frame.
+#' @param output_filename The filename for the written csv file, containing the
+#'     table of NA information. Default is "NA_Proportions.csv". Note: leaving
+#'     this default will cause the file to overwrite when run more than once in
+#'     the same directory.
+#'
+#' @returns A data frame with columns Column_Index, Column_Name, NA_Count,
+#'     NA_Proportion. The rows consist of the columns in the inputted data frame
+#'     which contain NAs. If there are no NAs in the inputted data frame, an
+#'     empty data frame is returned. Additionally, a csv is written to the
+#'     current working directory with the same information.
+#' @export
 analyze_nas <- function(df, output_filename = "NA_Proportions.csv") {
   na_info <- lapply(names(df), function(col_name) {
     column <- df[[col_name]]
--- R/handle.R
+++ R/handle.R
@@ -1,3 +1,11 @@
+#' Convert date variable.
+#'
+#' This function converts a RSA-911 date variable written in order YYYYMMDD as
+#'    one number to the appropriate date.
+#'
+#' @param x A date variable, written as a numeric YYYYMMDD.
+#' @returns The converted date variable.
+#' @export
 handle_date <- function(x){
   date <- as.Date(as.character(x), format = "%Y%m%d")
   return(date)
