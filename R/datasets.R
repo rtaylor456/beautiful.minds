1fb159bc14fa5579cf5d98c2c5bbd0f113cbc024
April Knight <a02367153@usu.edu>
2024-04-23 23:21:04 -0600

    Vinette

---
+++ R/datasets.R
@@ -0,0 +1,12 @@
+#' A Random Subset of RSA-911 PY2020exit
+#'
+#' A subset of data from the RSA-911 PY2020exit dataset
+#'
+#' @format ## `py_20_sample`
+#' A data frame with 300 rows and 307 columns:
+#' \describe{
+#'   \item{variables}{For a list of all 307 variables, check the website.}
+#'   ...
+#' }
+#' @source <https://rsa.ed.gov/performance/rsa-911-policy-directive>
+"py_20_sample"
